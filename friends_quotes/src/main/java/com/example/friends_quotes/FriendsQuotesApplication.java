4134b331b27be5af24bd79ba492728c70503025e
Fatimah Patel <fatimahpatel@Fatimahs-MacBook-Air.local>
2023-03-06 19:35:06 +0000

    new spring project

---
+++ friends_quotes/src/main/java/com/example/friends_quotes/FriendsQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.friends_quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsQuotesApplication.class, args);
+	}
+
+}
---
+++ friends_quotes/src/test/java/com/example/friends_quotes/FriendsQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.friends_quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
